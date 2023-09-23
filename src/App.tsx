9606bc64d245add4063268666ac3c9eef6e06936
Summer <swatson9008@yahoo.com>
2023-09-23 13:34:46 -0400

    converted numberButtons to strings and show func

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,25 @@ import NumberButtons from "./components/numberButtons";
 import SymbolButtons from "./components/symbolButton";
 
 function App() {
-  const [displayCount, setDisplay] = useState<Array<number | string>>([]);
+  const [displayCount, setDisplay] = useState<Array<string>>([]);
 
-  const showNumber = (numberValue: number) => {
-    setDisplay((prevDisplay) => [...prevDisplay, numberValue]);
+  const showNumber = (numberValue: string) => {
+    if (displayCount.length === 0 || isNaN(Number(displayCount[displayCount.length - 1]))) {
+      setDisplay((prevDisplay) => [...prevDisplay, numberValue]);
+    } else {
+      setDisplay((prevDisplay) => [
+        ...prevDisplay.slice(0, -1),
+        prevDisplay[prevDisplay.length - 1] + numberValue,
+      ]);
+    }
+    console.log(displayCount);
   };
+  
 
   const showSymbol = (symbolValue: string) => {
-    setDisplay((prevDisplay) => [...prevDisplay, symbolValue]);
+    if (displayCount.length === 0 || displayCount.length === 2){return alert("error!")}
+    if (displayCount.length === 3){console.log('success')}
+    else {setDisplay((prevDisplay) => [prevDisplay[0], symbolValue]); console.log(displayCount);}
   };
 
   return (
