595626b18690ceadf01d8ca7f8f779f08203d95c
Summer <swatson9008@yahoo.com>
2023-09-23 13:34:57 -0400

    changed to string

--- src/components/numberButtons.tsx
+++ src/components/numberButtons.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 interface NumberButtonsProps {
-  onClick: (numberValue: number) => void;
+  onClick: (numberValue: string) => void;
 }
 
 const NumberButtons: React.FC<NumberButtonsProps> = ({ onClick }) => {
-  const numbers: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const numbers: string[] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
   return (
     <div>
