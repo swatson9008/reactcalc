65f13754d9a44fe87055173943c20919812af173
Summer <swatson9008@yahoo.com>
2023-09-26 19:39:17 -0400

    implemented custom font

--- src/components/styles/displayMessage.styled.ts
+++ src/components/styles/displayMessage.styled.ts
@@ -8,7 +8,10 @@ export const StyledMessage = styled.div<GlobalStyleProps>`
   background-color: ${(props) => (props.dark ? "#2b2b2b" : "#c0ebbc")};
   color: ${(props) => (props.dark ? "#ebebeb" : "black")};
   padding: 20px;
+  font-family: 'VT323', monospace;
   font-weight: bold;
+  font-size: 18px;
+  letter-spacing: 1px; 
   margin: 10px;
   border-radius: 10px;
   text-align: right;
